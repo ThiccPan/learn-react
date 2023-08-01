840d7332da5c103a4c2440ab5ac0309584a45eb5
ThiccPan <ihilmiderian@gmail.com>
2023-08-01 18:17:56 +0700

    fso: 1d-async render

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -7,24 +7,33 @@ const Counter = () => {
         count2: 0
     })
 
+    const [total, setTotal] = useState(0)
+    const updated = 1
     const handleLeft = () => {
         setCount({
             ...count,
-            count1: count.count1 + 1,
+            count1: count.count1 + updated,
         });
+
+        setTotal(total + updated)
     }
 
     const handleRight = () => {
+        const updated = 1
         setCount({
             ...count,
-            count2: count.count2 + 1
+            count2: count.count2 + updated
         });
+
+        setTotal(total + updated)
     }
 
     return (
         <>
             <div>{count.count1}</div>
             <div>{count.count2}</div>
+            <hr />
+            <div>total: {total}</div>
             <button onClick={handleLeft}>+ 1 to left</button>
             <button onClick={handleRight}>+ 1 to left</button>
         </>
