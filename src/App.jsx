f112421ce4514836dcbd77c72d9f1956bb9397f8
ThiccPan <ihilmiderian@gmail.com>
2023-08-03 01:38:08 +0700

    fso: refactor component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { useState } from 'react';
 import './App.css'
 import PersonCard from "./PersonCard";
-import Counter from './Counter.jsx';
-import ArrayState from './ArrayState';
+import Counter from './components/Counter.jsx';
+import ArrayState from './components/ArrayState';
 
 class Employee {
 	constructor(name, age, departement) {
