2c9f9d8158d96e24f0b40582b703c722254995e3
ThiccPan <ihilmiderian@gmail.com>
2023-08-04 04:00:02 +0700

    fso: 2b-form

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,36 @@ import './App.css'
 import Counter from './components/Counter.jsx';
 import ArrayState from './components/ArrayState';
 import Note from './components/Note';
-import {personList, removePerson, editPerson} from './Person'
+import { personList, addPerson, removePerson, editPerson } from './Person'
 
 function App() {
 	const [personListState, setPersonList] = useState(personList)
+	const [newPerson, setNewPerson] = useState({})
+
+	const handlePersonChange = (event) => {
+		console.log(event.target.value)
+		setNewPerson({
+			name: event.target.value,
+		})
+	}
+
+	const onAddEvent = () => {
+		setPersonList(
+			addPerson(
+				personListState,
+				newPerson.name
+			)
+		)
+
+		console.log(personListState)
+	}
 
 	return (
 		<>
+			<span>
+				<input type="text" name="" id="" onChange={handlePersonChange} />
+				<button onClick={onAddEvent}>add person</button>
+			</span>
 			<button
 				onClick={() => editPerson(
 					personList,
@@ -27,7 +50,7 @@ function App() {
 				)}
 			</ul>
 
-			<Note/>
+			<Note />
 		</>
 	)
 }
--- src/Person.js
+++ src/Person.js
@@ -1,44 +1,56 @@
 let personList = [
-	{
-		id: 1,
-		name: "person 1",
-		age: 12,
-		salary: 1000
-	},
-	{
-		id: 2,
-		name: "person 2",
-		age: 14,
-		salary: 1000
-	}
+    {
+        id: 1,
+        name: "person 1",
+        age: 12,
+        salary: 1000
+    },
+    {
+        id: 2,
+        name: "person 2",
+        age: 14,
+        salary: 1000
+    }
 ]
 
+function addPerson(personList, name, age = 18, salary = 1000) {
+    return [
+        ...personList,
+        {
+            id: personList[personList.length-1].id + 1,
+            name: name,
+            age: age,
+            salary: salary
+        }
+    ]
+}
+
 function removePerson(personList, id, updateList) {
-	const newList = personList
-		.filter(person => {
-			console.log("id = ", person.id)
-			if (person.id !== id) {
-				console.log("match", person)
-				return person
-			}
-		})
-	console.log(newList)
-	updateList(newList)
-	console.log(personList)
+    const newList = personList
+        .filter(person => {
+            console.log("id = ", person.id)
+            if (person.id !== id) {
+                console.log("match", person)
+                return person
+            }
+        })
+    console.log(newList)
+    updateList(newList)
+    console.log(personList)
 }
 
 function editPerson(personList, id, updateList) {
-	const newList = personList.map((person) => {
-		console.log(person)
-		if (person.id === id) {
-			console.log("inside")
-			person.salary += 1000
-		}
-		return person
-	})
+    const newList = personList.map((person) => {
+        console.log(person)
+        if (person.id === id) {
+            console.log("inside")
+            person.salary += 1000
+        }
+        return person
+    })
 
-	console.log(newList)
-	updateList(newList)
+    console.log(newList)
+    updateList(newList)
 }
 
-export {personList, removePerson, editPerson}
+export { personList, addPerson, removePerson, editPerson }
