7992c8e4953138b99b6988d1ca3698d2616bb2fa
ThiccPan <ihilmiderian@gmail.com>
2023-08-21 05:11:02 +0700

    init server

--- server/index.js
+++ server/index.js
@@ -1,17 +1,36 @@
-import http from 'http'
 import fs from "fs";
+import express from "express";
 
 const PORT = 8080
 
 const sample = fs.readFileSync("db.json")
-let data = JSON.parse(sample)
+let data = JSON.parse(sample).notes
 console.log(data)
 
-const app = http.createServer((req, res) => {
-    res.writeHead(200, { 'content-type': 'text/plain' })
-    res.end(sample)
+const app = express()
+app.get('/', (req, res) => {
+    res.send(`pog online poggers`)
 })
 
-app.listen(PORT)
-console.log(`app started at ${PORT}`)
+app.get('/notes', (req, res) => {
+    res.send(data)
+})
 
+app.post('/notes', (req, res) => {
+    console.log(req.body)
+    res.send()
+})
+
+app.get('/notes/:id', (req, res) => {
+    const id = Number(req.params.id)
+    const resData = data.find((note) => note.id === id)
+    if (resData === undefined) {
+        res.status(404).send()
+        return
+    }
+    res.json(resData)
+})
+
+app.listen(PORT, () => {
+    console.log(`app started at ${PORT}`)
+})
