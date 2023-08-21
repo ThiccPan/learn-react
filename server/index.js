0401d6f1a8944bfed0ba2a40a563560249dccc8d
ThiccPan <ihilmiderian@gmail.com>
2023-08-22 03:08:53 +0700

    edit notes

--- server/index.js
+++ server/index.js
@@ -24,17 +24,20 @@ app.post('/notes', (req, res) => {
         res.sendStatus(400)
         return
     }
+
     let lastId = data.notes.at(data.notes.length - 1).id + 1
     let newNote = new Note(lastId, reqData.content, reqData.important)
+
     data.notes.push({ ...newNote })
     console.log(data)
+
     fs.writeFileSync("db.json", JSON.stringify(data))
     res
         .status(200)
         .json(
             {
                 "message": "success adding new note",
-                "data": {...newNote}
+                "data": { ...newNote }
             }
         )
 })
@@ -42,11 +45,39 @@ app.post('/notes', (req, res) => {
 app.get('/notes/:id', (req, res) => {
     const id = Number(req.params.id)
     const resData = data.find((note) => note.id === id)
+
     if (resData === undefined) {
         res.status(404).send()
         return
     }
+
     res.json(resData)
+})
+
+app.put('/notes/:id', (req, res) => {
+    const id = Number(req.params.id)
+    let reqBody = req.body
+    // find index
+    const resIdx = data.notes.findIndex((note) => note.id == id)
+    if (resIdx === -1) {
+        res.status(404).send()
+        return
+    }
+
+    let updated = data.notes.at(resIdx)
+
+    if (reqBody.content != undefined) {
+        updated.content = reqBody.content
+    }
+
+    if (reqBody.important != undefined) {
+        updated.important = reqBody.important
+    }
+
+    data.notes[resIdx] = updated
+    fs.writeFileSync("db.json", JSON.stringify(data))
+
+    res.send(data.notes[resIdx])
 })
 
 app.listen(PORT, () => {
